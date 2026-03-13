8932ac68904854192979c56104b6170a46f6e7f8
Markus Lenz <git@lenzma.de>
2026-03-13 09:28:31 +0100

    Chaged date/time into human readable format

--- resources/js/pages/Prices/Index.vue
+++ resources/js/pages/Prices/Index.vue
@@ -8,7 +8,15 @@ const props = defineProps({
 
 onMounted(() => {
 
-  const labels = props.prices.map(p => p.created_at)
+  const labels = props.prices.map(p => 
+  new Date(p.created_at).toLocaleString('de-DE', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit'
+  })
+)
   const data = props.prices.map(p => p.price)
 
   new Chart(document.getElementById('priceChart'), {
@@ -17,12 +25,24 @@ onMounted(() => {
       labels: labels,
       datasets: [{
         label: 'E10 Preis',
-        data: data
+        data: data,
+        fill: false,
+        tension: 0.1,
       }]
     }
   })
 
 })
+
+function formatDate(dateString) {
+  return new Date(dateString).toLocaleString('de-DE', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  })
+}
 </script>
 
 <template>
@@ -41,8 +61,8 @@ onMounted(() => {
   <tbody>
     <tr v-for="price in prices" :key="price.id">
       <td>{{ price.type }}</td>
-      <td>{{ price.price }}</td>
-      <td>{{ price.created_at }}</td>
+      <td>{{ price.price }} EUR</td>
+      <td>{{ formatDate(price.created_at) }}</td>
     </tr>
   </tbody>
 </table>
